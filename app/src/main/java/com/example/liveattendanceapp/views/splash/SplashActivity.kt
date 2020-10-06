771c49910e733d4585e58101c4c09a521de50435
USER <user@USERs-MacBook-Pro.local>
2020-10-06 13:49:49 +0700

    ui: add splash screen

---
+++ app/src/main/java/com/example/liveattendanceapp/views/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.example.liveattendanceapp.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.liveattendanceapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
